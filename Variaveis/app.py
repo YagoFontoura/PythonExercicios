16d87d177e4eb9a8c08d2a591c51d8d196013a3f
Yago B. Fontoura <yagofontoura222@gmail.com>
2022-05-01 14:28:28 -0300

    New Commit

---
+++ Variaveis/app.py
@@ -0,0 +1,15 @@
+velocidade_Internet = 10
+print(velocidade_Internet)
+
+# Tipos de valores
+# Número
+# Valores booleanos
+# String 
+
+A1,A2,A3 = 32,"Hello",True
+print(A1)
+print(A2)
+print(A3)
+print(type(A1))
+print(type(A2))
+print(type(A3))
